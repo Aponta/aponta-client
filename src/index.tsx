6c661db05e19ec5d89d3a13f974c860612f5f9f6
Marcelo Nogueira <mafealno@gmail.com>
2020-12-14 14:21:02 -0300

    retirado importações não usadas

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
-import cors from "cors";
-import app from "express";
 
 ReactDOM.render(
   <React.StrictMode>
